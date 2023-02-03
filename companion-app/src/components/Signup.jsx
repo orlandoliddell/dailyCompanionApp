c066e02ce048d13b9c24af6284cfbb6fe5296f5e
filthyningen <malikliddell@gmail.com>
2023-02-03 05:21:02 -0500

    Signin page is similar to sign up with slight differences. link ot sign up page.

--- companion-app/src/components/Signup.jsx
+++ companion-app/src/components/Signup.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Signup = () => {
 	return (
 		<div className="max-w-[700px] mx-auto my-16 p-4">
-			<h1 className="text-2xl font-bold py-2">Sign into your account</h1>
+			<h1 className="text-2xl font-bold py-2">Signup for free</h1>
 			<p className="py-2">
 				Already have an account?{" "}
 				<Link to="/" className="underline">
