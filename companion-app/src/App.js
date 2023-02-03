173eaf8103996528d56edf3442a047c4cacd0e58
filthyningen <malikliddell@gmail.com>
2023-02-03 09:27:38 -0500

    Installed framer motion for page route transitions. Need to figure out the use location function.

--- companion-app/src/App.js
+++ companion-app/src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react"
 import Signin from "./components/Signin"
 import Signup from "./components/Signup"
 import Account from "./components/Account"
-import { Route, Routes } from 'react-router-dom';
-
+import { Route, Routes } from "react-router-dom"
 function App() {
 	return (
-		<div className=''>
-			<h1 className="text-3xl font-bold underline text-center">
-				Hello world!
-			</h1>
-      <Routes>
-        <Route path='/' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
-      </Routes>
-		</div>
+			<div className="App">
+				<Routes >
+					<Route path="/" element={<Signin />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/account" element={<Account />} />
+				</Routes>
+			</div>
 	)
 }
 
--- companion-app/src/components/Signin.jsx
+++ companion-app/src/components/Signin.jsx
@@ -1,37 +1,45 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { motion as m} from 'framer-motion'; 
+import { motion as m } from "framer-motion"
 
 const Signin = () => {
 	return (
-		<m.div  initial={{ opacity:0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.75,ease: "easeOut"}}
-            className="max-w-[700px] mx-auto my-16 p-4">
-			<h1 className="text-2xl font-bold py-2">Sign into your account</h1>
-			<p className="py-2">
-				Don't have an account yet?{" "}
-				<Link to="/signup" className="underline">
-					Sign up
-				</Link>
-			</p>
+		<m.div
+			initial={{ y: "100%" }}
+			animate={{ y: "0%" }}
+			transition={{ duration: 0.5, delay: 0.5 }}
+			className="bg-orange-100 top-0 left-0 w-full h-full absolute"
+		>
+			<div className="max-w-[700px] mx-auto my-16 p-4">
+				<h1 className="text-2xl font-bold py-2">
+					Sign into your account
+				</h1>
+				<p className="py-2">
+					Don't have an account yet?{" "}
+					<Link to="/signup" className="underline">
+						Sign up
+					</Link>
+				</p>
 
-			<form>
-				<div className="flex flex-col py-2">
-					<label className="py-2 font-medium">Email Address</label>
-					<input className="border p-3" type="email" />
-				</div>
-				<div className="flex flex-col py-2">
-					<label className="py-2 font-medium">Password</label>
-					<input className="border p-3" type="password" />
-				</div>
-				<button
-					className=" border border-blue-500 w-full p-4 my-2 rounded-lg text-white cursor-pointer bg-gradient-to-r from-green-400 to-blue-500
+				<form>
+					<div className="flex flex-col py-2">
+						<label className="py-2 font-medium">
+							Email Address
+						</label>
+						<input className="border p-3" type="email" />
+					</div>
+					<div className="flex flex-col py-2">
+						<label className="py-2 font-medium">Password</label>
+						<input className="border p-3" type="password" />
+					</div>
+					<button
+						className=" border border-blue-500 w-full p-4 my-2 rounded-lg text-white cursor-pointer bg-gradient-to-r from-green-400 to-blue-500
                   hover:from-pink-500 hover:to-yellow-500 hover:scale-105 duration-300"
-				>
-					Login
-				</button>
-			</form>
+					>
+						Login
+					</button>
+				</form>
+			</div>
 		</m.div>
 	)
 }
