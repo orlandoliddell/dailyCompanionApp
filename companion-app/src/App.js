f647c269e1bf394923179603b1f95d43bc65a533
filthyningen <malikliddell@gmail.com>
2023-02-05 06:12:36 -0500

    Navbar.jsx created. very liimited during signin and signup, then opens up to the user once logged in.

--- companion-app/src/App.js
+++ companion-app/src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
 import AnimatedRoutes from "./components/AnimatedRoutes"
+import Navbar from "./components/Navbar"
 function App() {
 	return (
 		<div>
+			<Navbar />
 			<AnimatedRoutes />
 		</div>
 	)
