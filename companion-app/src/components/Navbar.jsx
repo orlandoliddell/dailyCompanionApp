6baeee2741082401c905d54b2d468414118d3730
filthyningen <malikliddell@gmail.com>
2023-02-09 06:55:25 -0500

    minor styling to navbar.

--- companion-app/src/components/Navbar.jsx
+++ companion-app/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const Navbar = () => {
-  return (
-    <div className='text-black z-20 relative'>Navbar</div>
-  )
+	return (
+		<div className="sticky text-black z-20 ">
+			<div className="p-4 flex text-left">Navbar</div>
+			<div className="p-4 flex text-left">About Me </div>
+			<div className="p-4 flex text-left">Navbar</div>
+		</div>
+	)
 }
 
 export default Navbar
